f1d4814f29d653941207c8e186aaa9896a5be7fa
Yograj07 <yograjra007@gmail.com>
2026-01-20 21:47:12 +0530

    Final home page designed and fixed UI improvments

--- home.php
+++ home.php
@@ -8,6 +8,7 @@
 
     <link rel="shortcut icon" href="./assets/images/favicon.png" type="image/x-icon">
     <link rel="stylesheet" href="assets/css/home.css">
+    <link rel="stylesheet" href="assets/css/footer.css">
     <script src="https://kit.fontawesome.com/b0b345bbc9.js" crossorigin="anonymous"></script>
     <script src="https://cdn.lordicon.com/lordicon.js"></script>
 
@@ -19,148 +20,104 @@
             <h1>ParkFlow <img src="./assets/images/favicon.png" alt="logo" width="40px"></h1>
         </div>
         <ul>
-            <li>Get Started</li>
-            <li>About Us</li>
+            <li>Login</li>
+            <li>SignUp</li>
         </ul>
     </nav>
+
+
     <main>
-
-        <div class="container">
-            <div class="main-texts">
-
-
-
-                <h2><lord-icon
-                        src="https://cdn.lordicon.com/bsmnglum.json"
-                        trigger="loop"
-                        delay="1000"
-                        colors="primary:#121331,secondary:#e86830"
-                        style="width:40px;height:40px">
-                    </lord-icon>Autonomus System That Make Sense. </h2>
-
-                <p>Revolutionize the way you park with our smart autonomous technology. Whether it is parallel or perpendicular parking, the system scans the environment and calculates the perfect trajectory to park your vehicle safely and efficiently. Step out, relax, and let the vehicle handle the rest.</p>
-
-                <a href="#">Explore Services<lord-icon
-                        src="https://cdn.lordicon.com/wjyqkiew.json"
-                        trigger="loop"
-                        delay="1000"
-                        colors="primary:#ffffff,secondary:#081943"
-                        style="width:35px;height:35px">
-                    </lord-icon></a>
-
-                <div class="con">
-                    <div class="dots"></div>
-                </div>
-
+        <div class="main-container">
+            <div class="side-headings-main">
+                <h1><span class="bg-alternate">Transform</span><span class="transform-icn"><lord-icon
+                            src="https://cdn.lordicon.com/nlowgkwj.json"
+                            trigger="loop"
+                            delay="1000"
+                            colors="secondary:#8F0177,secondary:#1b1091"
+                            style="width:70px;height:70px">
+                        </lord-icon></span> Your Parking Experience with <span class="bg-alternate">Smart</span> ParkFlow Technology.</h1>
+                <p>Effortless Parking, Elevated Experience instant navigation & contactless access at zero wait time.</p>
+                <a href="#">Get Started</a>
             </div>
-            <div class="boxes">
-                <div class="box">
-                    <span><lord-icon
-                            src="https://cdn.lordicon.com/wixhsrdu.json"
-                            trigger="loop"
-                            delay="1500"
-                            colors="primary:#121331,secondary:#ebe6ef,tertiary:#e86830"
-                            style="width:150px;height:150px">
-                        </lord-icon></span>
-                    <p>Smart Spot Detection</p>
-                </div>
-                <div class="box">
-                    <span><lord-icon
-                            src="https://cdn.lordicon.com/fgxwhgfp.json"
-                            trigger="loop"
-                            delay="1500"
-                            colors="primary:#121331,secondary:#e86830"
-                            style="width:150px;height:150px">
-                        </lord-icon></span>
-                    <p>Secure Services</p>
-                </div>
-                <div class="box">
-                    <span><lord-icon
-                            src="https://cdn.lordicon.com/onmwuuox.json"
-                            trigger="loop"
-                            delay="1500"
-                            colors="primary:#121331,secondary:#e86830"
-                            style="width:150px;height:150px">
-                        </lord-icon></span>
-                    <p>Access From Anywhere</p>
-                </div>
-                <div class="box">
-                    <span><lord-icon
-                            src="https://cdn.lordicon.com/mqgegihh.json"
-                            trigger="loop"
-                            delay="1500"
-                            colors="primary:#121331,secondary:#e86830"
-                            style="width:150px;height:150px">
-                        </lord-icon></span>
-                    <p>Easly Managable Service</p>
-                </div>
+            <div class="side-img">
+                <img src="./assets/images/illustrator_final.png" alt="ParkFlow">
             </div>
+        </div>
+        <div class="cover-dot">
+            <div class="dot"></div>
         </div>
     </main>
 
-
-
-    <div class="hr">
-        <hr>
-    </div>
     <section>
-        <div class="info-cart">
-
-            <div class="cart">
-
-                <lord-icon
-                    src="https://cdn.lordicon.com/ogjpwrxe.json"
-                    trigger="loop"
-                    delay="2000"
-                    colors="primary:#ffffff,secondary:#e86830"
-                    style="width:310px;height:310px">
-                </lord-icon>
-
-                <h2>User Freindly System</h2>
-            </div>
-
-            <div class="cart-texts">
-
-                <p>Our user-friendly autonomous parking system eliminates stress with a simple one-touch interface. Effortlessly navigate tight spaces as smart sensors and real-time precision handle every complex maneuver, ensuring a safe, seamless experience. By integrating advanced artificial intelligence with intuitive controls, we’ve made parking so effortless that you can step out and relax while your vehicle calculates the perfect trajectory, delivering unparalleled convenience and safety in every environment.</p>
-
-                <a href="#">Login to explore more</a>
-
-            </div>
-
-        </div>
-        <div class="hr">
-            <hr>
-        </div>
-
-        <div class="info-cart">
-
-            <div class="cart-texts">
-
-                <p>Our intuitive dashboard empowers you to manage your parking data with total ease. From reviewing your vehicle's parking history to customizing safety preferences, our streamlined interface ensures all your information is organized and accessible. By putting control back in your hands, we’ve created a transparent, user-centric ecosystem where managing your autonomous settings is as seamless as the parking itself.</p>
-
-                <div class="con">
-                    <div class="dots2"></div>
+        <div class="section-container">
+            <div class="items">
+                <div class="item-carts"><span><lord-icon
+                            src="https://cdn.lordicon.com/jectmwqf.json"
+                            trigger="loop"
+                            delay="1500"
+                            colors="secondary:#8F0177,primary:#1b1091"
+                            style="width:140px;height:140px">
+                        </lord-icon></span>
+                    <h5>Smart Spot Detection</h5>
                 </div>
-
+                <div class="item-carts"><span><lord-icon
+                            src="https://cdn.lordicon.com/edcgvlnw.json"
+                            trigger="loop"
+                            delay="1500"
+                            colors="primary:#1b1091,secondary:#8f0177"
+                            style="width:140px;height:140px">
+                        </lord-icon></span>
+                    <h5>Userfreindly System</h5>
+                </div>
+                <div class="item-carts"><span><lord-icon
+                            src="https://cdn.lordicon.com/fgxwhgfp.json"
+                            trigger="loop"
+                            delay="1500"
+                            colors="primary:#1b1091,secondary:#8f0177"
+                            style="width:140px;height:140px">
+                        </lord-icon></span>
+                    <h5>Secure Payment Service</h5>
+                </div>
+                <div class="item-carts"><span><lord-icon
+                            src="https://cdn.lordicon.com/onmwuuox.json"
+                            trigger="loop"
+                            delay="1500"
+                            colors="primary:#1b1091,secondary:#8f0177"
+                            style="width:140px;height:140px">
+                        </lord-icon></span>
+                    <h5>Access From Anywhere</h5>
+                </div>
             </div>
-
-            <div class="cart">
-
-                <lord-icon
-                    src="https://cdn.lordicon.com/edcgvlnw.json"
-                    trigger="loop"
-                    delay="2000"
-                    colors="primary:#ffffff,secondary:#e86830"
-                    style="width:300px;height:300px">
-                </lord-icon>
-
-                <h2>Manage Your Data easily</h2>
-            </div>
-
         </div>
-
     </section>
 
+    <div class="suggestion-container">
+
+
+        <form method="POST">
+            <h1>Get in touch</h1>
+            <hr>
+            <input type="text" placeholder="Enter first name" maxlength="20" id="texts" required>
+            <input type="text" placeholder="Enter last name" maxlength="20" id="texts" required>
+            <input type="tel" maxlength="10" placeholder="Enter mobile number" id="texts" required />
+            <textarea name="suggestion" id="" rows="3" cols="50" placeholder="Enter your valuable suggestion" required></textarea>
+
+            <button>
+                Send now
+            </button>
+
+        </form>
+        <div class="sugestion-illustraion">
+            <img src="./assets/images/get_in_touch_illustration.png" alt="img">
+        </div>
+    </div>
+    <div class="cover-dot ">
+        <div class="mrgbt"></div>
+    </div>
+
+    <?php
+    include "./includes/footer.php";
+    ?>
 
 </body>
 
---
+++ includes/footer.php
@@ -0,0 +1,66 @@
+<html lang="en">
+
+<head>
+    <script src="https://cdn.lordicon.com/lordicon.js"></script>
+    <link rel="stylesheet" href="../assets/css/footer.css">
+</head>
+
+<body>
+
+    <footer>
+        <div class="footer-container flex">
+            <div class="headings flex-col">
+                <h1>ParkFlow</h1>
+                <p>Revolutionize the way you park with our smart autonomous technology. Whether it is parallel or perpendicular parking, the system scans the environment and calculates the perfect trajectory to park your vehicle safely and efficiently. Step out, relax, and let the vehicle handle the rest.</p>
+            </div>
+            <div class="navigations">
+                <a href="#">Login</a>
+                <a href="#">SignUp</a>
+                <a href="#">About Us</a>
+            </div>
+            <div class="social-media-icons">
+                <span><lord-icon
+                        src="https://cdn.lordicon.com/tgyvxauj.json"
+                        trigger="loop"
+                        delay="1500"
+                        stroke="bold"
+                        colors="primary:#060771,secondary:#0000000"
+                        style="width:40px;height:40px">
+                    </lord-icon></span>
+
+                <span><lord-icon
+                        src="https://cdn.lordicon.com/yizwahhw.json"
+                        trigger="loop"
+                        delay="1500"
+                        stroke="bold"
+                        colors="primary:#060771,secondary:#1b1091"
+                        style="width:40px;height:40px">
+                    </lord-icon></span>
+
+                <span><lord-icon
+                        src="https://cdn.lordicon.com/qgebwute.json"
+                        trigger="loop"
+                        delay="1500"
+                        stroke="bold"
+                        colors="primary:#060771,secondary:#000000"
+                        style="width:40px;height:40px">
+                    </lord-icon></span>
+
+                <span><lord-icon
+                        src="https://cdn.lordicon.com/bfoumeno.json"
+                        trigger="loop"
+                        delay="1500"
+                        stroke="bold"
+                        colors="primary:#060771,secondary:#1b1091"
+                        style="width:40px;height:40px">
+                    </lord-icon></span>
+            </div>
+        </div>
+        <div class="rights">
+            &copy; 2026 All Rights Reserved | ParkFlow AI's(Ltd) | Made in India
+        </div>
+    </footer>
+
+</body>
+
+</html>
